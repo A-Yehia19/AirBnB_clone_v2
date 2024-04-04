31ae0342bab8c339215f4a61d7a44ec640cbecbf
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-04-04 16:27:00 +0200

    style fixed

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,15 +5,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def root_route():
     """ root route """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def HBNB_route():
     """ HBNB route """
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def C_route(text):
