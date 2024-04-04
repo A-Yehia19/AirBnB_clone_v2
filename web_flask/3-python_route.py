ee498bb823bec9fb8da918c159442077a1c757ba
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-04-04 16:34:44 +0200

    init run

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" route module """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def root_route():
+    """ root route """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB_route():
+    """ HBNB route """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_route(text):
+    """ C route """
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    """ Python route """
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
